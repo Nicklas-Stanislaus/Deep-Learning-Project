373401c94e3e1be7d622e9433821be16b36453a4
Nicklas Stanislaus <99009841+Nicklas-Stanislaus@users.noreply.github.com>
2022-11-18 14:37:45 +0100

    Create generate_inputs.py

---
+++ Code/generate_inputs.py
@@ -0,0 +1,23 @@
+def generate_inputs(nonogram_picture):    
+    out = []
+    for mode in ("rows", "columns"): 
+        
+        #If working with columns transpose nonogram picture
+        if mode == "columns": 
+            nonogram_picture = list(map(list, zip(*nonogram_picture)))
+        
+        inputs = []
+        for line in nonogram_picture:  
+            # Create the input for each line
+            one_seqs = filter(None, "".join(map(str,line)).split("0"))
+            line_input = [len(x) for x in list(one_seqs)]
+            
+            # Add padding
+            inputs.append([0]*(len(line)-len(line_input)) + line_input)
+        
+        #If working with columns transpose the input back to right dimensions
+        if mode == "columns": 
+            inputs = list(map(list, zip(*inputs)))
+        
+        out.append(inputs)
+    return out
