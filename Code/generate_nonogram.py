b3e58db5b57f37c5995e9ca78cb6c45296d23f2d
Nicklas Stanislaus <99009841+Nicklas-Stanislaus@users.noreply.github.com>
2022-11-24 15:28:37 +0100

    added code for generating nonograms

---
+++ Code/generate_nonogram.py
@@ -0,0 +1,34 @@
+import numpy as np
+def generate_nonogram(d_i = 0.2, d_v = 0.3, d_h = 0.3, colums=5, rows = 5, sim_number = 1):
+    '''
+    generate fixed size nonograms
+    
+    d_i  = overall accept rate
+    d_h = extra acceptrate if left cell is occupied.
+    d_v = extra acceptrate if above cell is occupied.
+    '''
+    n_nonograms = []
+    for _ in range(sim_number):
+        nonogram = []
+        for i in range(rows):
+            row = []
+            left = False
+            for j in range(colums):
+                r = d_i
+                if i != 0 and last_row[j] == 1:
+                    r += d_v  
+                if left:
+                    r += d_h
+                var = np.random.rand()
+                
+                if var < r: 
+                    row.append(1)
+                    left = True 
+                else:
+                    row.append(0)
+                    left = False
+                    
+            nonogram.append(row)
+            last_row = row
+        n_nonograms.append(nonogram)
+    return np.array(n_nonograms)
